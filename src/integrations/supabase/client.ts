baaa11ff023a6229d76093bc389f8711409c1f14
adhemultazam <adhescco@gmail.com>
2025-05-10 21:03:49 +0800

    Update client.ts

--- src/integrations/supabase/client.ts
+++ src/integrations/supabase/client.ts
@@ -10,6 +10,6 @@ export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
   auth: {
     persistSession: true,
     autoRefreshToken: true,
-    storage: localStorage
+    detectSessionInUrl: true,
   }
 });
